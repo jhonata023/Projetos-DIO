7f856a10c71c5b0d5dd5510449e88fe7596b7866
Medeiros <137403955+jhonata023@users.noreply.github.com>
2023-07-26 21:40:05 -0300

    Update Sistema bancário.py

--- Sistema bancário.py
+++ Sistema bancário.py
@@ -1,4 +1,5 @@
 saldo = 0
+cont = 0
 print('Bem vindo ao nosso sistema bancário.')
 while True:
     print('=-' * 6, 'MENÚ', '=-' * 6)
@@ -14,17 +15,28 @@ while True:
         while True:
             saque = int(input(f'''Seu saldo é de R${saldo:.2f}.
 Digite o valor a ser sacado: R$'''))
-            if saque > saldo:
+            if cont > 2:
+                print('Seu limite de saque foi esgotado.')
+                break
+            elif saque > saldo:
                 print('Saldo insuficiente. Por favor, tente novamente.')
+            elif saque > 500:
+                print('Seu limite de saque é de R$500,00. Por favor, tente novamente.')
             else:
                 saldo -= saque
                 print(f'Seu agora é R${saldo:.2f}')
+                cont += 1
                 break
     if opção == 2:
         print(f'Seu saldo é de R${saldo:.2f}')
     elif opção == 3:
-        depósito = int(input('Digite o valor a ser depositado: R$'))
-        saldo += depósito
+        while True:
+            depósito = int(input('Digite o valor a ser depositado: R$'))
+            if depósito <= 0:
+                print('Opção inválida. Por favor, insira um valor positivo.')
+            elif depósito > 0:
+                saldo += depósito
+                break
     elif opção == 0:
         print('Obrigado por utilizar nosso sistema. Volte logo!')
         break
