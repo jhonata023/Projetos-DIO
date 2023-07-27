c578894256b80ac7f357aca22ae7b50a7e96aad0
Medeiros <137403955+jhonata023@users.noreply.github.com>
2023-07-26 22:01:22 -0300

    Update Sistema bancário.py

--- Sistema bancário.py
+++ Sistema bancário.py
@@ -1,5 +1,6 @@
 saldo = 0
 cont = 0
+extrato = ''
 print('Bem vindo ao nosso sistema bancário.')
 while True:
     print('=-' * 6, 'MENÚ', '=-' * 6)
@@ -13,7 +14,7 @@ while True:
 
     if opção == 1:
         while True:
-            saque = int(input(f'''Seu saldo é de R${saldo:.2f}.
+            saque = float(input(f'''Seu saldo é de R${saldo:.2f}.
 Digite o valor a ser sacado: R$'''))
             if cont > 2:
                 print('Seu limite de saque foi esgotado.')
@@ -24,18 +25,25 @@ Digite o valor a ser sacado: R$'''))
                 print('Seu limite de saque é de R$500,00. Por favor, tente novamente.')
             else:
                 saldo -= saque
+                extrato += f'Saque: R${saque:.2f}\n'
                 print(f'Seu agora é R${saldo:.2f}')
                 cont += 1
                 break
     if opção == 2:
+        print('~' * 12)
+        print('EXTRATO')
+        print('~' * 12)
+        print(extrato)
         print(f'Seu saldo é de R${saldo:.2f}')
+        print('~' * 31)
     elif opção == 3:
         while True:
-            depósito = int(input('Digite o valor a ser depositado: R$'))
+            depósito = float(input('Digite o valor a ser depositado: R$'))
             if depósito <= 0:
                 print('Opção inválida. Por favor, insira um valor positivo.')
             elif depósito > 0:
                 saldo += depósito
+                extrato += f'Depósito: {depósito:.2f}\n'
                 break
     elif opção == 0:
         print('Obrigado por utilizar nosso sistema. Volte logo!')
